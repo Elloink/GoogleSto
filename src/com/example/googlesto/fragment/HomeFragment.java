20651b47baffa86cf69f546096f58135a1e25e04
threelu <215154863@qq.com>
2016-01-21 23:52:38 +0800

    第五次 解析json数据
    解析json数据：解析json数据 碰到大括号{}用JSONObject,碰到中括号[]用JSONArray

--- src/com/example/googlesto/fragment/HomeFragment.java
+++ src/com/example/googlesto/fragment/HomeFragment.java
@@ -1,11 +1,12 @@
 package com.example.googlesto.fragment;
 
+import java.util.List;
 import java.util.Random;
 
 import com.example.googlesto.R;
+import com.example.googlesto.domin.AppInfo;
 import com.example.googlesto.protocol.HomeProtocol;
 import com.example.googlesto.view.LoadingPage.LoadResult;
-
 
 //import android.R;
 import android.os.Bundle;
@@ -23,12 +24,13 @@ import android.widget.FrameLayout.LayoutParams;
 import android.widget.TextView;
 
 public class HomeFragment extends BaseFragment {
-	//当此fragment挂载的activity创建的时候调用show（）；因为刚加载的时候，fragment没有滑动，所以不能调用show()
+	// 当此fragment挂载的activity创建的时候调用show（）；因为刚加载的时候，fragment没有滑动，所以不能调用show()
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		show();
 	}
+
 	// 创建成功界面
 	public View createSuccessView() {
 		TextView tv = new TextView(getActivity());
@@ -36,12 +38,21 @@ public class HomeFragment extends BaseFragment {
 		tv.setTextSize(30);
 		return tv;
 	}
-	
+
 	public LoadResult load() {
 		HomeProtocol hProtocol = new HomeProtocol();
-		hProtocol.load(0);
-		return LoadResult.success;
+		List<AppInfo> load = hProtocol.load(0);
+		return checkData(load);
 	}
 
+	private LoadResult checkData(List<AppInfo> load) {
+		if (load==null) {
+			return LoadResult.error;
+		}else if(load.size()==0){
+			return LoadResult.empty;
+		}else {
+			return LoadResult.success;
+		}
+	}
 
 }
--- src/com/example/googlesto/protocol/HomeProtocol.java
+++ src/com/example/googlesto/protocol/HomeProtocol.java
@@ -8,10 +8,17 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.widget.Toast;
 
 import com.example.googlesto.BaseApplication;
+import com.example.googlesto.domin.AppInfo;
 import com.example.googlesto.http.HttpHelper;
 import com.example.googlesto.http.HttpHelper.HttpResult;
 import com.example.googlesto.tools.FileUtis;
@@ -26,27 +33,50 @@ import com.lidroid.xutils.util.IOUtils;
 
 public class HomeProtocol {
 	String result = null;
-
-	public void load(int index) {
-		String json = loadLocal(index);
+	String json=null;
+	public List<AppInfo> load(int index) {
 		if (json == null) {
 			loadServer(index);
-		}else {
-			System.out.println("复用了本地缓存");
 		}
+	    json = loadLocal(index);
 		if (json != null) {
-			paserJson(json);
+			return paserJson(json);
+		} else {
+			return null;
 		}
 
 	}
 
 	/**
-	 * 解析json数据
+	 * 解析json数据 碰到大括号{}用JSONObject,碰到中括号[]用JSONArray
 	 * 
 	 * @param json
 	 *            对象
 	 */
-	private void paserJson(String json) {
+	private List<AppInfo> paserJson(String json) {
+		List<AppInfo> list = new ArrayList<AppInfo>();
+		try {
+			JSONObject jsonObject = new JSONObject(json);
+			JSONArray jsonArray = jsonObject.getJSONArray("list");
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonObject2 = jsonArray.getJSONObject(i);
+				long id = jsonObject2.getLong("id");
+				String name = jsonObject2.getString("name");
+				String packageName = jsonObject2.getString("packageName");
+				String iconUrl = jsonObject2.getString("iconUrl");
+				float stars = Float.parseFloat(jsonObject2.getString("stars"));
+				long size = jsonObject2.getLong("size");
+				String downloadUrl = jsonObject2.getString("downloadUrl");
+				String des = jsonObject2.getString("des");
+				AppInfo appinfo = new AppInfo(id, name, packageName, iconUrl,
+						stars, size, downloadUrl, des);
+				list.add(appinfo);
+			}
+			return list;
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
 
 	}
 
@@ -86,32 +116,31 @@ public class HomeProtocol {
 	 * @return json
 	 */
 	private String loadLocal(int index) {
-		File dir = FileUtis.getCacheDir();
-		// 在第一行写一个过期时间
-		File file = new File(dir, "home_"+index);
-		FileReader fr;
-		BufferedReader bf;
+		//  如果发现文件已经过期了 就不要再去复用缓存了
+		File dir=FileUtis.getCacheDir();// 获取缓存所在的文件夹
+		File file = new File(dir,"home_" + index); 
 		try {
-			fr = new FileReader(file);
-			 bf = new BufferedReader(fr);
-			 long outofdate = Long.parseLong(bf.readLine());
-			 if (System.currentTimeMillis()>outofdate) {
-				return null;
-			}else {
+			FileReader fr=new FileReader(file);
+			BufferedReader br=new BufferedReader(fr);
+			long outOfDate = Long.parseLong(br.readLine());
+//			if(System.currentTimeMillis()>outOfDate){
+//				return null;
+//			}else{
 				String str=null;
-				StringWriter sw = new StringWriter();
-				while ((bf.readLine())!=null) {
+				StringWriter sw=new StringWriter();
+				while((str=br.readLine())!=null){
+				
 					sw.write(str);
 				}
 				return sw.toString();
-			}
+//			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
-		
-		
-		return null;
 	}
+
 
 	public int getIndex(int index) {
 		return index;
@@ -137,7 +166,7 @@ public class HomeProtocol {
 			@Override
 			public void onSuccess(ResponseInfo<String> responseInfo) {
 				result = (String) responseInfo.result;
-				saveLocal(result, getIndex(index));//保存到本地
+				saveLocal(result, getIndex(index));// 保存到本地
 			}
 		});
 	}
