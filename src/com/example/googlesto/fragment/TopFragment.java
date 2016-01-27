93dcaf55243f2567104de31db28c6a1a9c1a2fbf
threelu <215154863@qq.com>
2016-01-27 23:24:44 +0800

    第二十   排行界面数据解析
    解析排行界面数据

--- src/com/example/googlesto/fragment/TopFragment.java
+++ src/com/example/googlesto/fragment/TopFragment.java
@@ -1,28 +1,45 @@
 package com.example.googlesto.fragment;
 
+import java.util.List;
 
+import com.example.googlesto.R;
+import com.example.googlesto.protocol.TopProtocol;
+import com.example.googlesto.tools.UIUtils;
 import com.example.googlesto.view.LoadingPage.LoadResult;
 
+import android.bluetooth.le.ScanCallback;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class TopFragment extends BaseFragment {
+	private List<String> data;
 
 	@Override
 	public View createSuccessView() {
-		// TODO Auto-generated method stub
-		return null;
+		ScrollView scrollView = new ScrollView(UIUtils.getContext());
+		scrollView.setBackgroundResource(R.drawable.grid_item_bg_normal);
+		LinearLayout layout = new LinearLayout(UIUtils.getContext());
+		layout.setOrientation(LinearLayout.VERTICAL);
+		for (int i = 0; i < data.size(); i++) {
+			TextView textView = new TextView(UIUtils.getContext());
+			textView.setText(data.get(i));
+			layout.addView(textView);
+		}
+		scrollView.addView(layout);
+		return scrollView;
 	}
 
 	@Override
 	public LoadResult load() {
-		// TODO Auto-generated method stub
-		return LoadResult.error;
+		TopProtocol protocol = new TopProtocol();
+		data = protocol.load(0);
+		return checkData(data);
 	}
-	
 
 }
